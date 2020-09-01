72e3e694c170e5200a5bcc0e0650becb0ba9364d
Victoria Blackburn <victoriablackburn@Victorias-Air.lan>
2020-08-31 20:12:03 -0700

    updated files

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 // array of questions for user
-var inquirer= require("inquirer");
+const inquirer= require("inquirer");
+const fs =require("fs");
+const util=require("util");
 
-inquirer.prompt([
+const generateMarkdown= require("./utils/generateMarkdown");
+
+const questions=[
     {
         type: "input",
         message: "What is your project title?",
-        name: "title"
+        name: "title",
     },
     {
         type: "input",
         message: "GitHub username?",
-        name: "GitHub"
+        name: "GitHub",
     },
     {
         type: "input",
         message: "Email address?",
-        name: "email"
+        name: "email",
     },
     {
         type: "input",
         message: "Describe your project:",
-        name: "description"
+        name: "description",
     },
     {
         type: "input",
         message: "Installation instructions:",
-        name: "installation"
+        name: "installation",
     },
     {
         type: "input",
         message: "Usage information:",
-        name: "usage"
+        name: "usage",
     },
     {
         type: "input",
         message: "Contribution guidelines:",
-        name: "contribution"
+        name: "contribution",
     },
     {
         type: "input",
         message: "Test instructions:",
-        name: "test"
+        name: "test",
     },
     {
         type: "checkbox",
@@ -50,37 +54,31 @@ inquirer.prompt([
         "Copyright", 
         "Trademark", 
         ]
-    },
-]).then((userResponse)=>{
+    },]; 
+function init(){
+inquirer.prompt(questions).then((userAnswers)=>{
 
-    const data = writeHTML(userResponse)
-    fs.writeFile("index.html", data, function(){
-      console.log("success")
+    const readMe=generateMarkdown(userAnswers);
+    fs.writeFile("README.md", readMe, function(err){
+        if(err){
+            return console.log(err);
+        }
+        console.log("successfully load to README.md")
     })
-  });
+    console.log(userAnswers)
+})
+.catch((err)=>console.log(err));
+}
+init();
+// .then((userResponse)=>{
 
+//     const data = writeHTML(userResponse)
+//     fs.writeFile("index.html", data, function(){
+//       console.log("success")
+//     })
+//   });
 
-function writeHTML(userObj){
-    console.log("inside of HTML function", userObj)
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ReadMe: ${userObj.title}</title>
-    </head>
-    <body>
-        <h1>Description ${userObj.description}</h1>
-        <h2>Installation ${userObj.installation}</h2>
-        <p>Usage ${userObj.usage}</p>
-        <p>Contributing ${userObj.contribution}</p>
-        <p>Tests ${userObj.test}</p>
-        <h2>Questions: ${userObj.GitHub}, "\n" ${userObj.email}</h2>
-        <h3>Table of Contents:</h3>
-    </body>
-    </html>
-}`  
+   
 // const questions = [
 
 // ];
@@ -89,10 +87,10 @@ function writeHTML(userObj){
 // function writeToFile(fileName, data) {
 // }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `## Title:
+   ${data.title}
+  ## GitHub Username: 
+  ${data.GitHub}
+  ## Email Address:
+  ${data.email}
+  ## Describe Your Project:
+  ${data.description}
+  ## Installation Instructions:
+  ${data.installation}
+  ## Usage Information:
+  ${data.usage}
+  ## Contribution Guidelines:
+  ${data.contribution}
+  ## Test Instructions:
+  ${data.test}
+  ## Liscensing Agreement:
+  ${data.stack}
 `;
 }
 
